2bb47575249a79995a2394d120ce18e00aefc620
eng-arvind <41833207+eng-arvind@users.noreply.github.com>
2019-12-16 14:21:04 +0530

    method

---
+++ method.java
@@ -0,0 +1,13 @@
+class method
+{
+public static void main(String[] args)
+{
+System.out.println("Method call");
+Mymethod();
+System.out.println("Method successfully executed!");
+}
+private static void Mymethod()
+{
+System.out.println("Print inside of method");
+}
+}
