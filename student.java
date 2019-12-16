109ef4bbed9a56a58e5e4e5db908385d4479fd2f
eng-arvind <41833207+eng-arvind@users.noreply.github.com>
2019-12-16 14:22:47 +0530

    package

---
+++ student.java
@@ -0,0 +1,9 @@
+import Hello.*;
+class student
+{
+public static void main(String []args)
+{
+World obj=new World();
+obj.sum();
+}
+}
---
+++ world.java
@@ -0,0 +1,12 @@
+package Hello;
+public class World
+{
+int a;
+public int b;
+private int c;
+protected int d;
+public void sum()
+{
+System.out.print(a+b+c+d);
+}
+}
